0424f19a295fde77e73c7d71ffe3607a26707e63
ndass-droid <ndass@g.hmc.edu>
2026-04-30 16:43:35 -0700

    fixed kinda

--- Constants.py
+++ Constants.py
@@ -2,10 +2,8 @@ import numpy as np
 
 DEFAULT_SAMPLE_RATE = 22050
 DEFAULT_HOP_SIZE_SAMPLES = 512
-<<<<<<< HEAD
-LAMBDA = 0.2
-=======
-LAMBDA = 0.2
+
+LAMBDA = .5
 
 TRAINING_TRANSTIONS = np.array([[0, 1], [1, 1], [1, 0]])
->>>>>>> cf91c80ee6559aeb5178dde3872d68704fccc117
+
