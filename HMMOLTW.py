693a1bf1033db0cfb7f46684353fe86aee4d3976
MorganPulling <mpulling@hmc.edu>
2026-04-30 17:33:31 -0700

    Correct HMM Weighting

--- HMMOLTW.py
+++ HMMOLTW.py
@@ -288,9 +288,7 @@ def StepStreamingDTW(
         - RunningState:               current StreamingDTW state
         - ReferenceChroma:            (FeatureDim, RefFrameCount) reference feature matrix
         - NewQueryFrame:              (FeatureDim,) current query chroma vector
-        - ViterbiNormalizedLogProbs:  (StateCount,) normalized log-probs from the current Viterbi step
         - SearchHalfWidth:            half-width of the active window around CurrentReferenceEstimate
-        - HMMWeight:                  scaling factor for the Viterbi cost bias (0 = pure DTW)
     Returns:
         - Updated StreamingDTWRunningState with new row costs and reference frame estimate
     """
@@ -377,7 +375,7 @@ def ProcessNextFrame(
     StreamingDTWEstimate = UpdatedStreamingDTWState.CurrentReferenceEstimate
     ViterbiEstimate = UpdatedViterbiState.CurrentReferenceEstimate
 
-    WeightedMidpointState = int(StreamingDTWEstimate + HMMWeight * ViterbiEstimate) // 2
+    WeightedMidpointState = int(StreamingDTWEstimate + HMMWeight * (ViterbiEstimate - StreamingDTWEstimate))
 
     return WeightedMidpointState, UpdatedViterbiState, UpdatedStreamingDTWState
 
